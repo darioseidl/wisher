ce3f67cbb47df0a3e97e2d1301ccb2deaa5b4837
Dario Seidl <darioseidl@fastmail.fm>
2025-12-16 23:59:26 +0100

    New day

--- h.js
+++ h.js
@@ -5,8 +5,8 @@ const timer = document.getElementById('timer');
 const wisher = document.getElementById('wisher');
 let urlParam = new URLSearchParams(data);
 
-let n = urlParam.get('n') || "Körpersprache";
-let d = urlParam.get('d') || "15.12";
+let n = urlParam.get('n') || "Kuschelkühe";
+let d = urlParam.get('d') || "16.12";
 let m = urlParam.get('m') || "Dario";
 
 const input = d; // Example input
